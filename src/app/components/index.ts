5383da58b7d9ba8bdacb20974490b6af29902811
Michael Wright <mike.j.wright@outlook.com>
2023-05-07 20:05:08 -0400

    remove files

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,5 +1,8 @@
 export { Footer } from './Footer';
 export { Header } from './Header';
-export { TodoList } from './TodoList';
-export { TodoItem } from './TodoItem';
-export { TodoTextInput } from './TodoTextInput';
+// export { TodoList } from './TodoList';
+// export { TodoItem } from './TodoItem';
+// export { Note } from './Note';
+// export { ColorPicker } from './ColorPicker';
+// export { NoteInput } from './NoteInput';
+// export { TodoTextInput } from './TodoTextInput';
--- src/app/containers/App/index.tsx
+++ src/app/containers/App/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { KeyboardEvent, useState } from 'react';
 import style from './style.css';
 import { RouteComponentProps } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
 import { useTodoActions } from 'app/actions';
 import { RootState } from 'app/reducers';
 import { TodoModel } from 'app/models';
-import { Header, TodoList, Footer } from 'app/components';
+import { Header, Footer } from 'app/components';
 
 const FILTER_VALUES = (Object.keys(TodoModel.Filter) as (keyof typeof TodoModel.Filter)[]).map(
   (key) => TodoModel.Filter[key]
@@ -17,47 +17,88 @@ const FILTER_FUNCTIONS: Record<TodoModel.Filter, (todo: TodoModel) => boolean> =
   [TodoModel.Filter.SHOW_COMPLETED]: (todo) => todo.completed
 };
 
+type NoteType = {
+  text: string;
+  bgColor: string;
+}
+
 export namespace App {
   export interface Props extends RouteComponentProps<void> {}
 }
 
 export const App = ({ history, location }: App.Props) => {
-  const dispatch = useDispatch();
-  const todoActions = useTodoActions(dispatch);
-  const { todos, filter } = useSelector((state: RootState) => {
-    const hash = location?.hash?.replace('#', '');
-    return {
-      todos: state.todos,
-      filter: FILTER_VALUES.find((value) => value === hash) ?? TodoModel.Filter.SHOW_ALL
-    };
-  });
 
-  const handleClearCompleted = React.useCallback((): void => {
-    todoActions.clearCompleted();
-  }, [todoActions]);
+  //const dispatch = useDispatch();
+  // const todoActions = useTodoActions(dispatch);
+  // const { todos, filter } = useSelector((state: RootState) => {
+  //   const hash = location?.hash?.replace('#', '');
+  //   return {
+  //     todos: state.todos,
+  //     filter: FILTER_VALUES.find((value) => value === hash) ?? TodoModel.Filter.SHOW_ALL
+  //   };
+  // });
 
-  const handleFilterChange = React.useCallback(
-    (filter: TodoModel.Filter): void => {
-      history.push(`#${filter}`);
-    },
-    [history]
-  );
+  // const handleClearCompleted = React.useCallback((): void => {
+  //   todoActions.clearCompleted();
+  // }, [todoActions]);
 
-  const filteredTodos = React.useMemo(() => (filter ? todos.filter(FILTER_FUNCTIONS[filter]) : todos), [todos, filter]);
-  const activeCount = React.useMemo(() => todos.filter((todo) => !todo.completed).length, [todos]);
-  const completedCount = React.useMemo(() => todos.filter((todo) => todo.completed).length, [todos]);
+  // const handleFilterChange = React.useCallback(
+  //   (filter: TodoModel.Filter): void => {
+  //     history.push(`#${filter}`);
+  //   },
+  //   [history]
+  // );
+
+  // const filteredTodos = React.useMemo(() => (filter ? todos.filter(FILTER_FUNCTIONS[filter]) : todos), [todos, filter]);
+  // const activeCount = React.useMemo(() => todos.filter((todo) => !todo.completed).length, [todos]);
+  // const completedCount = React.useMemo(() => todos.filter((todo) => todo.completed).length, [todos]);
+
+
+  //////
+
+  const [selectedColor, setSelectedColor] = useState('blue');
+  const [noteText, setNoteText] = useState('');
+  const [notes, setNotes] = useState<NoteType[]>([{
+    text: 'This is my note',
+    bgColor: 'blue'
+  }]);
+
+  const handleTextInputChange = (text: string) => {
+    setNoteText(text);
+  };
+
+  const handleColorSelect = (text: string) => {
+    setSelectedColor(text);
+  };
+
+  const handleDeleteNote = (index: number) => {
+    let updatedNotes = [...notes];
+    updatedNotes.splice(index, 1);
+    setNotes(updatedNotes);
+  }
+
+  const submitNote = () => {
+    const newNote: NoteType = {
+      text: noteText,
+      bgColor: selectedColor
+    }
+    setNotes([...notes, newNote])
+    setNoteText('');
+  }
+
+  // const handleKeyDown = (event: React.KeyboardEvent<HTMLButtonElement>) => {
+  //   if (event.key === 'Enter') { // 13 is the keycode for the Enter key
+  //     event.preventDefault(); // prevent the default behavior of the Enter key
+  //     const submitBtn = document.getElementById('submitBtn');
+  //     if (submitBtn) {
+  //       submitBtn.click();
+  //     }
+  //   }
+  // }
 
   return (
-    <div className={style.normal}>
-      <Header addTodo={todoActions.addTodo} />
-      <TodoList todos={filteredTodos} actions={todoActions} />
-      <Footer
-        filter={filter}
-        activeCount={activeCount}
-        completedCount={completedCount}
-        onClickClearCompleted={handleClearCompleted}
-        onClickFilter={handleFilterChange}
-      />
+    <div>
+      <p>Hello</p>
     </div>
   );
 };
